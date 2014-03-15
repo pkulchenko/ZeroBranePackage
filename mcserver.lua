53df5e30541ca88b173398a18592299b0ee6bf40
madmaxoft <github@xoft.cz>
2014-03-15 19:36:53 +0100

    MCServer: Automatically start debugging by adding a dummy file.
    When the debugger is started, a new Lua file is added into the project that starts the debugger. The file is removed again once debugging finishes.

--- mcserver.lua
+++ mcserver.lua
@@ -4,7 +4,7 @@
 local function MakeMCServerInterpreter(a_InterpreterPostfix, a_ExePostfix)
 	assert(type(a_InterpreterPostfix) == "string")
 	assert(type(a_ExePostfix) == "string")
-	
+
 	return
 	{
 		name = "MCServer" .. a_InterpreterPostfix,
@@ -27,22 +27,38 @@ local function MakeMCServerInterpreter(a_InterpreterPostfix, a_ExePostfix)
 
 			-- The executable name depends on the debug / non-debug build mode, it can have a postfix
 			ExeName:SetName("MCServer" .. a_ExePostfix)
-			
+
 			-- Executable has a .exe ext on Windows
 			if (ide.osname == 'Windows') then
 				ExeName:SetExt("exe")
 			end
-			
+
 			-- Add a "nooutbuf" cmdline param to the server, causing it to call setvbuf to disable output buffering:
 			local Cmd = ExeName:GetFullPath() .. " nooutbuf"
-			
+
 			-- Force ZBS not to hide MCS window, save and restore previous state:
 			local SavedUnhideConsoleWindow = ide.config.unhidewindow.ConsoleWindowClass
 			ide.config.unhidewindow.ConsoleWindowClass = 1  -- show if hidden
-			local RestoreUnhide = function()
-				ide.config.unhidewindow.ConsoleWindowClass = SavedUnhideConsoleWindow
+			
+			-- Create the @EnableMobDebug.lua file so that the MCS plugin starts the debugging session, when loaded:
+			local EnablerPath = wx.wxFileName(wfilename)
+			EnablerPath:SetName("@EnableMobDebug")
+			EnablerPath:SetExt("lua")
+			local f = io.open(EnablerPath:GetFullPath(), "w")
+			if (f ~= nil) then
+				f:write([[require("mobdebug").start()]])
+				f:close()
 			end
 			
+			-- Create the closure to call upon debugging finish:
+			local OnFinished = function()
+				-- Restore the Unhide status:
+				ide.config.unhidewindow.ConsoleWindowClass = SavedUnhideConsoleWindow
+			
+				-- Remove the @EnableMobDebug.lua file:
+				os.remove(EnablerPath:GetFullPath())
+			end
+
 			-- Run the server:
 			local pid = CommandLineRun(
 				Cmd,                    -- Command to run
@@ -51,27 +67,35 @@ local function MakeMCServerInterpreter(a_InterpreterPostfix, a_ExePostfix)
 				true,                   -- Add a no-hide flag to WX
 				nil,                    -- StringCallback, whatever that is
 				nil,                    -- UID to identify this running program; nil to auto-assign
-				RestoreUnhide           -- Callback to call once the debuggee terminates
+				OnFinished              -- Callback to call once the debuggee terminates
 			)
 		end,
-		
+
 		hasdebugger = true,
 	}
 end
 
+
+
+
+
 return {
-  name = "MCServer integration",
-  description = "Integration with MCServer - the custom C++ minecraft server.",
-  author = "Mattes D (https://github.com/madmaxoft)",
-  version = 0.1,
+	name = "MCServer integration",
+	description = "Integration with MCServer - the custom C++ minecraft server.",
+	author = "Mattes D (https://github.com/madmaxoft)",
+	version = 0.2,
 
-  onRegister = function(self)
-    ide:AddInterpreter("mcserver_debug", MakeMCServerInterpreter(" - debug mode", "_debug"))
-    ide:AddInterpreter("mcserver_release", MakeMCServerInterpreter(" - release mode", ""))
-  end,
+	onRegister = function(self)
+		ide:AddInterpreter("mcserver_debug", MakeMCServerInterpreter(" - debug mode", "_debug"))
+		ide:AddInterpreter("mcserver_release", MakeMCServerInterpreter(" - release mode", ""))
+	end,
 
-  onUnRegister = function(self)
-    ide:RemoveInterpreter("mcserver_debug")
-    ide:RemoveInterpreter("mcserver_release")
-  end,
+	onUnRegister = function(self)
+		ide:RemoveInterpreter("mcserver_debug")
+		ide:RemoveInterpreter("mcserver_release")
+	end,
 }
+
+
+
+
