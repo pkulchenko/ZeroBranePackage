c8bd884095de59c2bdd0c07a6c59d6be6b30f62b
Paul Kulchenko <paul@kulchenko.com>
2014-05-29 14:38:55 -0700

    Updated shortcut for `overtype` to avoid conflict.

--- overtype.lua
+++ overtype.lua
@@ -5,14 +5,14 @@ return {
   name = "Overtype on/off",
   description = "Allows to switch overtyping on/off on systems that don't provide shortcut for that.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
   dependencies = 0.50,
 
   onRegister = function(self)
     local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
     local pos = self:GetConfig().insertat and
       self:GetConfig().insertat-1 or menu:GetMenuItemCount()
-    menuid = menu:InsertCheckItem(pos, id, "Overtype"..KSC(id, "Ctrl-I"))
+    menuid = menu:InsertCheckItem(pos, id, "Overtype"..KSC(id, "Alt-Shift-I"))
     ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED,
       function (event) GetEditor():SetOvertype(event:IsChecked()) end)
     ide:GetMainFrame():Connect(id, wx.wxEVT_UPDATE_UI,
