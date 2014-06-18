394241179234ccc0dafd09d2aac58d6506a60abe
Paul Kulchenko <paul@kulchenko.com>
2014-06-17 20:29:57 -0700

    Added `XML syntax highlighting` plugin.

---
+++ xml.lua
@@ -0,0 +1,49 @@
+-- Copyright 2014 Paul Kulchenko, ZeroBrane LLC; All rights reserved
+
+local spec = {
+  exts = {"xml"},
+  lexer = wxstc.wxSTC_LEX_XML,
+  apitype = "xml",
+  stylingbits = 7,
+
+  lexerstyleconvert = {
+    text = {wxstc.wxSTC_H_DEFAULT,},
+    comment = {wxstc.wxSTC_H_COMMENT,},
+    stringeol = {wxstc.wxSTC_HJ_STRINGEOL,},
+    number = {wxstc.wxSTC_H_NUMBER,},
+    stringtxt = {
+      wxstc.wxSTC_H_DOUBLESTRING,
+      wxstc.wxSTC_H_SINGLESTRING,
+    },
+    lexerdef= {
+      wxstc.wxSTC_H_OTHER,
+      wxstc.wxSTC_H_ENTITY,
+      wxstc.wxSTC_H_VALUE,
+    },
+    keywords0 = {
+      wxstc.wxSTC_H_TAG,
+      wxstc.wxSTC_H_ATTRIBUTE,
+    },
+    keywords1 = {wxstc.wxSTC_H_TAGUNKNOWN,
+      wxstc.wxSTC_H_ATTRIBUTEUNKNOWN,
+    },
+    keywords2 = {wxstc.wxSTC_H_SCRIPT,},
+    keywords3 = {wxstc.wxSTC_LUA_WORD,},
+    keywords4 = {wxstc.wxSTC_LUA_WORD1,},
+    keywords5 = {wxstc.wxSTC_LUA_WORD2,},
+    preprocessor= {wxstc.wxSTC_LUA_PREPROCESSOR,},
+  },
+
+  keywords = {
+  },
+}
+
+return {
+  name = "XML syntax highlighting",
+  description = "XML syntax highlighting",
+  author = "Paul Kulchenko",
+  version = 0.1,
+
+  onRegister = function(self) ide:AddSpec("xml", spec) end,
+  onUnRegister = function(self) ide:RemoveSpec("xml") end,
+}
