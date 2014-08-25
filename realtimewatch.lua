0336fa88f1012f5094afc233446f4e8c3e2a4957
Paul Kulchenko <paul@kulchenko.com>
2014-08-24 18:50:34 -0700

    Updated `Realtime watch` plugin to use `AddWatch` method.

--- realtimewatch.lua
+++ realtimewatch.lua
@@ -5,7 +5,8 @@ return {
   name = "Real-time watches",
   description = "Displays real-time values during debugging.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
+  dependencies = 0.71,
 
   onRegister = function()
     local config = ide:GetConfig()
@@ -16,23 +17,6 @@ return {
     config.debugger.outputfilter = function(s)
       local label, value = s:match('"(.-)%s*=%s*"\t(.+)')
       if not label or not value then return s end
-
-      local watchCtrl = ide.debugger.watchCtrl
-
-      local row = lastrow[label]
-      if not row or watchCtrl:GetItemText() ~= label then
-        row = nil -- reset in case it's not found in the control
-        for idx = 0, watchCtrl:GetItemCount() - 1 do
-          if label == watchCtrl:GetItemText(idx) then
-            row = idx
-            break
-          end
-        end
-      end
-
-      if not row then
-        row = watchCtrl:InsertItem(watchCtrl:GetItemCount(), label)
-      end
 
       local num = tonumber(value)
       -- for numbers report min/max/count/avg
@@ -48,9 +32,7 @@ return {
           :format(num, stat[3], stat[4], stat[1], stat[2]/stat[1])
       end
 
-      watchCtrl:SetItem(row, 0, label)
-      watchCtrl:SetItem(row, 1, (value:gsub("\t", "; ")))
-
+      ide:AddWatch(label, (value:gsub("\t", "; ")))
       return
     end
   end,
