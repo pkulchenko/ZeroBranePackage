4a450bd05a32e422d8932f5de3c8344a018d0290
Paul Kulchenko <paul@kulchenko.com>
2014-11-22 20:55:17 -0800

    Updated clone view to save the document when saving the clone.

--- cloneview.lua
+++ cloneview.lua
@@ -1,8 +1,27 @@
+local clones = {}
 return {
   name = "Clone view",
   description = "Clones the current editor tab.",
   author = "Paul Kulchenko",
-  version = 0.11,
+  version = 0.12,
+  dependencies = 0.91,
+
+  -- don't offer to save cloned tabs
+  onEditorPreSave = function(self, editor, filepath)
+    if clones[editor] then
+      -- save the original document
+      ide:GetDocument(clones[editor].editor):Save()
+      return false -- don't save the clone
+    end
+  end,
+
+  -- release document pointer for closed tabs
+  onEditorClose = function(self, editor)
+    if clones[editor] then
+      clones[editor].editor:ReleaseDocument(clones[editor].pointer)
+      clones[editor] = nil
+    end
+  end,
 
   onMenuEditorTab = function(self, menu, notebook, event, index)
     local idvert = ID(self.fname..".clone.vert")
@@ -12,6 +31,8 @@ return {
       local e1 = ide:GetEditor(index)
       local e2 = NewFile("clone: "..ide:GetDocument(e1):GetFileName())
       local docpointer = e1:GetDocPointer()
+      e1:AddRefDocument(docpointer)
+      clones[e2] = {editor = e1, pointer = docpointer}
       e2:SetDocPointer(docpointer)
       ide:GetEditorNotebook():Split(notebook:GetSelection(),
         event:GetId() == idhorz and wx.wxRIGHT or wx.wxBOTTOM)
