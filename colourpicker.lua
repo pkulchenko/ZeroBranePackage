7c78eada8adb0ff760e95ce1d6d5f8a2087843b8
Paul Kulchenko <paul@kulchenko.com>
2016-05-07 11:20:02 -0700

    Updated `ColourPicker` plugin to add menu item (in the `View` menu).

--- colourpicker.lua
+++ colourpicker.lua
@@ -1,33 +1,35 @@
 local id = ID("colourpicker.insertcolour")
+local function insertcolour(event)
+  local editor = ide:GetEditor()
+  if not editor then return end
+  local rgb = editor:GetSelectedText():match("(%d+,%s*%d+,%s*%d+)")
+  local colour = wx.wxColour(rgb and "rgb("..rgb..")" or wx.wxBLACK)
+  local newcolour = wx.wxGetColourFromUser(ide:GetMainFrame(), colour)
+  if newcolour:Ok() then -- user selected some colour
+    local newtext = newcolour:GetAsString(wx.wxC2S_CSS_SYNTAX):match("%d+,%s*%d+,%s*%d+") or ""
+    ide:GetEditor():ReplaceSelection(newtext)
+  end
+end
+
 return {
   name = "Colour picker",
   description = "Select color to insert in the document",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
   dependencies = 1.0,
 
   onRegister = function()
-    -- SetAccelerator is only available in 1.31+, so check for it
-    if ide.SetAccelerator and KSC(id) > "" then ide:SetAccelerator(id, KSC(id)) end
-    ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED, function(event)
-        local editor = ide:GetEditor()
-        if not editor then return end
-        local rgb = editor:GetSelectedText():match("(%d+,%s*%d+,%s*%d+)")
-        local colour = wx.wxColour(rgb and "rgb("..rgb..")" or wx.wxBLACK)
-        local newcolour = wx.wxGetColourFromUser(ide:GetMainFrame(), colour)
-        if newcolour:Ok() then -- user selected some colour
-          local newtext = newcolour:GetAsString(wx.wxC2S_CSS_SYNTAX):match("%d+,%s*%d+,%s*%d+") or ""
-          ide:GetEditor():ReplaceSelection(newtext)
-        end
-      end)
+    local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&View")))
+    menu:AppendSeparator()
+    menu:Append(id, "Colour Picker Window"..KSC(id))
+    ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED, insertcolour)
   end,
   onUnRegister = function()
-    if ide.SetAccelerator then ide:SetAccelerator(id, nil) end
-    ide:GetMainFrame():Disconnect(id, wx.wxEVT_COMMAND_MENU_SELECTED)
+    ide:RemoveMenuItem(id)
   end,
 
   onMenuEditor = function(self, menu, editor, event)
     menu:AppendSeparator()
-    menu:Append(id, "Select colour to insert"..KSC(id))
+    menu:Append(id, "Insert Colour"..KSC(id))
   end
 }
