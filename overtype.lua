e5145c5a686ccb766ac854849c0f9012c480dfe4
Paul Kulchenko <paul@kulchenko.com>
2016-08-04 09:16:46 -0700

    Improved plugins to handle no open editors (closes #38).

--- overtype.lua
+++ overtype.lua
@@ -1,27 +1,28 @@
-local G = ...
-local id = G.ID("overtype.overtype")
-local menuid
+local id = ID("overtype.overtype")
 return {
   name = "Overtype on/off",
   description = "Allows to switch overtyping on/off on systems that don't provide shortcut for that.",
   author = "Paul Kulchenko",
-  version = 0.2,
+  version = 0.3,
   dependencies = 0.50,
 
   onRegister = function(self)
     local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
     local pos = self:GetConfig().insertat and
       self:GetConfig().insertat-1 or menu:GetMenuItemCount()
-    menuid = menu:InsertCheckItem(pos, id, "Overtype"..KSC(id, "Alt-Shift-I"))
-    ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED,
-      function (event) GetEditor():SetOvertype(event:IsChecked()) end)
-    ide:GetMainFrame():Connect(id, wx.wxEVT_UPDATE_UI,
-      function (event) event:Check(GetEditor():GetOvertype()) end)
+    menu:InsertCheckItem(pos, id, "Overtype"..KSC(id, "Alt-Shift-I"))
+    ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED, function(event)
+        local ed = ide:GetEditor()
+        if ed then ed:SetOvertype(event:IsChecked()) end
+      end)
+    ide:GetMainFrame():Connect(id, wx.wxEVT_UPDATE_UI, function(event)
+        local ed = ide:GetEditor()
+        event:Check(ed and ed:GetOvertype())
+        event:Enable(ed ~= nil)
+      end)
   end,
 
   onUnRegister = function(self)
-    local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
-    ide:GetMainFrame():Disconnect(id, wx.wxID_ANY, wx.wxID_ANY)
-    if menuid then menu:Destroy(menuid) end
+    ide:RemoveMenuItem(id)
   end,
 }
--- tildemenu.lua
+++ tildemenu.lua
@@ -1,22 +1,23 @@
-local G = ...
-local id = G.ID("tildemenu.tildemenu")
-local menuid
+local id = ID("tildemenu.tildemenu")
 return {
   name = "Tilde",
   description = "Allows to enter tilde (~) on keyboards that may not have it.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
 
   onRegister = function(self)
     local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
-    menuid = menu:Append(id, "Tilde\tAlt-'")
-    ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED,
-      function () GetEditor():AddText("~") end)
+    menu:Append(id, "Tilde\tAlt-'")
+    ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED, function()
+        local ed = ide:GetEditor()
+        if ed then ed:AddText("~") end
+      end)
+    ide:GetMainFrame():Connect(id, wx.wxEVT_UPDATE_UI, function(event)
+        event:Enable(ide:GetEditor() ~= nil)
+      end)
   end,
 
   onUnRegister = function(self)
-    local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
-    ide:GetMainFrame():Disconnect(id, wx.wxID_ANY, wx.wxEVT_COMMAND_MENU_SELECTED)
-    if menuid then menu:Destroy(menuid) end
+    ide:RemoveMenuItem(id)
   end,
 }
