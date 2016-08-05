18f5da3936af9a60ca29cc999cc9a18439980da7
Paul Kulchenko <paul@kulchenko.com>
2016-08-05 09:36:26 -0700

    Updated sample `popupmenu` plugin to handle no open editors (#38)

--- popupmenu.lua
+++ popupmenu.lua
@@ -1,27 +1,25 @@
-local G = ...
-local id = G.ID("popupmenu.popupshow")
-local iditem = G.ID("popupmenu.popupitem")
-local menuid
+local id = ID("popupmenu.popupshow")
+local iditem = ID("popupmenu.popupitem")
 return {
   name = "Sample plugin with popup menu",
   description = "Sample plugin showing how to setup and use popup menu.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
+  dependencies = 1.30,
 
   onRegister = function(self)
     -- add menu item that will activate popup menu
     local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
-    menuid = menu:Append(id, "Show Popup\tCtrl-Alt-T")
+    menu:Append(id, "Show Popup\tCtrl-Alt-T")
     ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED, function()
-      GetEditor():AddPendingEvent(wx.wxContextMenuEvent(wx.wxEVT_CONTEXT_MENU))
-    end)
+        local editor = ide:GetEditor()
+        if editor then editor:AddPendingEvent(wx.wxContextMenuEvent(wx.wxEVT_CONTEXT_MENU)) end
+      end)
   end,
 
   onUnRegister = function(self)
     -- remove added menu item when plugin is unregistered
-    local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Edit")))
-    ide:GetMainFrame():Disconnect(id, wx.wxID_ANY, wx.wxEVT_COMMAND_MENU_SELECTED)
-    if menuid then menu:Destroy(menuid) end
+    ide:RemoveMenuItem(id)
   end,
 
   onMenuEditor = function(self, menu, editor, event)
@@ -31,6 +29,6 @@ return {
 
     -- attach a function to the added menu item
     editor:Connect(iditem, wx.wxEVT_COMMAND_MENU_SELECTED,
-      function(event) DisplayOutputLn("Selected popup item {") end)
+      function(event) ide:Print("Selected popup item {") end)
   end
 }
