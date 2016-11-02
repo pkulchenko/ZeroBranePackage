4362b42abdfe77cc544b3964eede6ddd49190bdb
Paul Kulchenko <paul@kulchenko.com>
2016-11-01 21:42:33 -0700

    Updated `Local help menu` package to use current API.

--- localhelpmenu.lua
+++ localhelpmenu.lua
@@ -1,15 +1,14 @@
-local G = ...
-local id = G.ID("localhelpmenu.localhelpmenu")
-local menuid
+local id = ID("localhelpmenu.localhelpmenu")
 return {
   name = "Local Lua help",
   description = "Adds local help option to the menu.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
+  dependencies = "1.30",
 
   onRegister = function(self)
-    local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Help")))
-    menuid = menu:Append(id, "Lua Documentation")
+    local menu = ide:FindTopMenu("&Help")
+    menu:Append(id, "Lua Documentation")
     ide:GetMainFrame():Connect(id, wx.wxEVT_COMMAND_MENU_SELECTED,
       function (event) wx.wxLaunchDefaultBrowser(self:GetConfig().index, 0) end)
     ide:GetMainFrame():Connect(id, wx.wxEVT_UPDATE_UI,
@@ -17,8 +16,6 @@ return {
   end,
 
   onUnRegister = function(self)
-    local menu = ide:GetMenuBar():GetMenu(ide:GetMenuBar():FindMenu(TR("&Help")))
-    ide:GetMainFrame():Disconnect(id, wx.wxID_ANY, wx.wxID_ANY)
-    if menuid then menu:Destroy(menuid) end
+    ide:RemoveMenuItem(id)
   end,
 }
