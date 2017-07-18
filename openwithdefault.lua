48bcbc801259ab8caf4263d9fc1f4fe187d1447d
Paul Kulchenko <paul@kulchenko.com>
2017-07-18 09:00:06 -0700

    Updated `openwithdefault` to not activate on known extensions (closes pkulchenko/ZeroBraneStudio#775).

--- openwithdefault.lua
+++ openwithdefault.lua
@@ -2,13 +2,15 @@ return {
   name = "Open With Default",
   description = "Opens file with Default Program when activated.",
   author = "Paul Kulchenko",
-  version = 0.1,
-  dependencies = 0.51,
+  version = 0.2,
+  dependencies = 1.0,
 
   onFiletreeActivate = function(self, tree, event, item_id)
     local fname = tree:GetItemText(item_id)
-    local ext = '.'..wx.wxFileName(fname):GetExt()
-    local ft = wx.wxTheMimeTypesManager:GetFileTypeFromExtension(ext)
+    local ext = wx.wxFileName(fname):GetExt()
+    -- don't activate for known extensions
+    if #(ide:GetKnownExtensions(ext)) == 1 then return end
+    local ft = wx.wxTheMimeTypesManager:GetFileTypeFromExtension('.'..ext)
     if ft then
       tree:SelectItem(item_id)
       tree:AddPendingEvent(wx.wxCommandEvent(wx.wxEVT_COMMAND_MENU_SELECTED, ID_OPENEXTENSION))
