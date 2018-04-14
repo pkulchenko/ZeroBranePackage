c104fbbe517408dfbf5a24a44241d38675435a30
RobertL <robertlzj@users.noreply.github.com>
2018-04-14 18:05:39 +0800

    Revert "Change behavior when selection"
    This reverts commit e93edf0863aa84a3501b5ecc5e56db0f813e8afd.

--- autodelimitersurroundselection.lua
+++ autodelimitersurroundselection.lua
@@ -5,7 +5,7 @@
 local cpairs = {
   ['('] = ')', ['['] = ']', ['{'] = '}', ['"'] = '"', ["'"] = "'"}
 local closing = [[)}]'"]]
-local selection,spos,epos = ""
+local selection = ""
 return {
   name = "Auto-insertion of delimiters",
   description = [[Extends auto-insertion of delimiters (), {}, [], '', and "" to add selection and removal of standalone pairs.]],
@@ -27,7 +27,6 @@ return {
       end
     end
     selection = editor:GetSelectedText()
-		spos,epos = editor:GetSelectionStart(), editor:GetSelectionEnd()
   end,
   onEditorCharAdded = function(self, editor, event)
     local keycode = event:GetKey()
@@ -47,9 +46,7 @@ return {
     elseif cpairs[char] then
       if editor:GetCharAt(curpos - 2) ~= hyphen and editor:GetCharAt(curpos - 3) ~= hyphen then
       -- if the entered matches opening delimiter, then insert the pair
-				editor:InsertText(-1, selection .. cpairs[char])
-				editor:SetSelectionStart(spos)
-				editor:SetSelectionEnd(epos+2)
+        editor:InsertText(-1, selection .. cpairs[char])
       end
     end
   end,
