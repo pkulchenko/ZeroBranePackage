c10fe6c0e30838bc5a709b9261cbdf7132345132
Paul Kulchenko <paul@kulchenko.com>
2018-05-09 22:29:30 -0700

    Updated `edgemark` for consistency.

--- edgemark.lua
+++ edgemark.lua
@@ -1,17 +1,18 @@
-local plugin = {
+local function markEdge(self, editor)
+  local config = self.GetConfig and self:GetConfig()
+  editor:SetEdgeMode(wxstc.wxSTC_EDGE_LINE)
+  editor:SetEdgeColumn(config and config.column or 80)
+  if config and config.color then
+    editor:SetEdgeColour(wx.wxColour((table.unpack or unpack)(config.color)))
+  end
+end
+
+return {
   name = "Mark edge",
   description = "Marks column edge for long lines.",
   author = "Paul Kulchenko",
-  version = 0.2,
+  version = 0.21,
 
-  onEditorLoad = function(self, editor)
-    local config = self.GetConfig and self:GetConfig()
-    editor:SetEdgeMode(wxstc.wxSTC_EDGE_LINE)
-    editor:SetEdgeColumn(config and config.column or 80)
-    if config and config.color then
-      editor:SetEdgeColour(wx.wxColour((table.unpack or unpack)(config.color)))
-    end
-  end,
+  onEditorLoad = markEdge,
+  onEditorNew = markEdge,
 }
-plugin.onEditorNew = plugin.onEditorLoad
-return plugin
