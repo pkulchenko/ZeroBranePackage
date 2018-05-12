326053b739e8be7c75499a682e943c817d8c99ec
Paul Kulchenko <paul@kulchenko.com>
2018-05-11 22:10:50 -0700

    Updated `editorautofocusbymouse` for consistency.

--- editorautofocusbymouse.lua
+++ editorautofocusbymouse.lua
@@ -1,12 +1,13 @@
-local function focusOnEnterWindow(editor)
+local function focusOnEnterWindow(self, editor)
   editor:Connect(wx.wxEVT_ENTER_WINDOW, function() editor:SetFocus() end)
 end
+
 return {
   name = "Editor auto-focus by mouse",
   description = "Moves focus to an editor tab the mouse is over.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.11,
 
-  onEditorLoad = function(self, editor) focusOnEnterWindow(editor) end,
-  onEditorNew = function(self, editor) focusOnEnterWindow(editor) end,
+  onEditorLoad = focusOnEnterWindow,
+  onEditorNew = focusOnEnterWindow,
 }
