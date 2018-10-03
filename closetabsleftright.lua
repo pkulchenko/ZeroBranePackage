0fb02cd97613672b0a9f146cc70428aa48f604f6
Paul Kulchenko <paul@kulchenko.com>
2018-10-02 22:22:50 -0700

    Updated `closetabsleftright` plugin to stop closing on canceling save.

--- closetabsleftright.lua
+++ closetabsleftright.lua
@@ -2,8 +2,8 @@ return {
   name = "Close tabs left and right",
   description = "Closes editor tabs to the left and to the right of the current one.",
   author = "Paul Kulchenko",
-  version = 0.11,
-  dependencies = "1.00",
+  version = 0.12,
+  dependencies = "1.60",
 
   onMenuEditorTab = function(self, menu, notebook, event, index)
     local idleft = ID(self.fname..".left")
@@ -15,10 +15,14 @@ return {
     menu:Enable(idleft, index > 0)
     menu:Enable(idright, index < notebook:GetPageCount()-1)
     notebook:Connect(idleft, wx.wxEVT_COMMAND_MENU_SELECTED, function()
-        for _ = 0, index-1 do ide:GetDocument(notebook:GetPage(0)):Close() end
+        for _ = 0, index-1 do
+          if not ide:GetDocument(notebook:GetPage(0)):Close() then break end
+        end
       end)
     notebook:Connect(idright, wx.wxEVT_COMMAND_MENU_SELECTED, function()
-        for _ = index+1, notebook:GetPageCount()-1 do ide:GetDocument(notebook:GetPage(index+1)):Close() end
+        for _ = index+1, notebook:GetPageCount()-1 do
+          if not ide:GetDocument(notebook:GetPage(index+1)):Close() then break end
+        end
       end)
   end,
 }
