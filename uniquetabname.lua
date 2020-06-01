e3e5e620f0774673580fe2338abefb6ec5d011d8
Paul Kulchenko <paul@kulchenko.com>
2020-06-01 14:11:26 -0700

    Fixed conflict between uniquetabname and cloneview (closes pkulchenko/ZeroBraneStudio#1060).
    Saving a cloned tab led to infinite loop to resolve naming conflict.

--- uniquetabname.lua
+++ uniquetabname.lua
@@ -4,25 +4,40 @@ local function makeUnique(exeditor)
   for id, doc in pairs(ide:GetDocuments()) do
     if doc:GetEditor() ~= exeditor and doc:GetFileName() and doc:GetFilePath() then
       local fn = doc:GetFileName()
-      docs[fn] = docs[fn] or {}
-      table.insert(docs[fn], {doc = doc, parts = wx.wxFileName(doc:GetFilePath()):GetDirs()})
+      local fpath = doc:GetFilePath()
+      local uniquepath = true
+      if docs[fn] then
+        for _, tab in pairs(docs[fn]) do
+          if tab.path == fpath then uniquepath = false end
+        end
+      end
+      -- only count duplicates if they are for different paths
+      -- this excludes clones and other situations when the paths match
+      if uniquepath then
+        docs[fn] = docs[fn] or {}
+        table.insert(docs[fn], {doc = doc, path = fpath, parts = wx.wxFileName(doc:GetFilePath()):GetDirs()})
+      end
     end
   end
 
   while true do
     local updated = false
+    local newdocs = {}
     for fn, tabs in pairs(docs) do
       if #tabs > 1 then -- conflicting name
         updated = true
-        docs[fn] = nil
+        newdocs[fn] = nil
         for _, tab in ipairs(tabs) do
-          local fn = (table.remove(tab.parts) or '?').. sep .. fn
-          docs[fn] = docs[fn] or {}
-          table.insert(docs[fn], tab)
+          local fn = (table.remove(tab.parts) or '?') .. sep .. fn
+          if not docs[fn] then
+            newdocs[fn] = newdocs[fn] or {}
+            table.insert(newdocs[fn], tab)
+          end
         end
       end
     end
     if not updated then break end
+    docs = newdocs
   end
 
   -- update all labels as some might have lost their conflicts
@@ -34,7 +49,7 @@ return {
   name = "Unique tabname",
   description = "Updates editor tab names to always stay unique.",
   author = "Paul Kulchenko",
-  version = 0.1,
+  version = 0.2,
 
   onEditorLoad = function(self) makeUnique() end,
   onEditorSave = function(self) makeUnique() end,
